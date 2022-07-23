8174ce041b82c2c805cfe8da0b4485099854390c
Megha Dogra <76041569+Megha-prog@users.noreply.github.com>
2022-07-23 22:07:46 +0530

    Add files via upload

--- springprofileapp/src/main/java/com/example/springprofileapp/controller/UserController.java
+++ springprofileapp/src/main/java/com/example/springprofileapp/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.springprofileapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+//import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -17,15 +18,20 @@ public class UserController {
         this.userService = userService;
     }
 
+
     @PostMapping("/addusers")
-    public List<User> PersistUser(@RequestBody List<User> users) {
-        return PersistUser(users);
+    public String PersistUser(@RequestBody List<User> user) {
+        return userService.PersistUser(user);
     }
 
-    @GetMapping("/users")
-    public List<User> getUser( List<User> users) {
+    @GetMapping("/user")
+    public List<User> getUser() {
         return userService.getAllUsers();
     }
-
+    @GetMapping("/user/userid")
+    private User getUsers(@PathVariable("userid") Integer userid)
+    {
+        return userService.getUsersById(userid);
+    }
 
 }
--- springprofileapp/src/main/java/com/example/springprofileapp/service/UserService.java
+++ springprofileapp/src/main/java/com/example/springprofileapp/service/UserService.java
@@ -4,10 +4,13 @@ import com.example.springprofileapp.model.User;
 
 import com.example.springprofileapp.reposatory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class   UserService {
@@ -17,21 +20,29 @@ public class   UserService {
     UserRepository userRepository;
 
 
-
-    public List<User>  getAllUsers() {
-
-       return userRepository.findAll();
-
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+        userRepository.findAll().forEach(users1 -> users.add(users1));
+        return users;
     }
 
     public String PersistUser(List<User> users) {
         if (users.size() == 0 || users.isEmpty()) {
-            Assert.notNull(users,"user can't be null");
+            Assert.notNull(users, "user can't be null");
         }
         userRepository.saveAll(users);
         return "Successfully done " + users.size();
     }
-}
+
+    public User getUsersById(Integer userid) {
+        Optional<User> userData = userRepository.findById(userid);
+        if (userData.isEmpty()) {
+            Assert.notNull(userid, "user can't be null");
+        }
+        return userRepository.findById(userid).get();
+    }
+    }
+
 
 
 
